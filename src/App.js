bb34c9fe2c95825fb6b1070a92464ffe692f86cb
NRS\nrsha <nrshakib007@gmail.com>
2022-08-22 18:08:29 +0600

    added onAuthStateChange

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import LogIn from './components/LogIn/LogIn';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
         
       </Routes>
     </div>
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const LogIn = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Log In</h3>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='Your Mail' />
+                <br />
+                <input type="password" name="" id="" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Log In" />
+            </form>
         </div>
     );
 };
